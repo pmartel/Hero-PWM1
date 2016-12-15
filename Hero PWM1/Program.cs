fec554bb5ee88458d36e6fef944f2f858596e484
Phil Martel <pmartel@alum.mit.edu>
2016-12-15 17:56:39 -0500

    Using left y-axis to control servo position.  I set the servo's range emperically.

--- Hero PWM1/Program.cs
+++ Hero PWM1/Program.cs
@@ -17,9 +17,15 @@ namespace Hero_PWM1
         public static void Main()
         {
             uint period = 50000; //period between pulses
-            uint duration = 1500; //duration of pulse
-            int delta, cross;
-            CTRE.GamepadValues val = new CTRE.GamepadValues();
+            // I noticed that the normal 1000 to 2000 ms delay does not allow full range on the servo I'm using
+            // so I'm setting up high/low and center values to give a fuller range
+            // eventually create a Servo class with these values and a PWM
+            uint servoLow = 600;
+            uint servoHi = 2400;
+            uint servoCenter = (uint)((servoLow + servoHi) / 2);
+            uint duration = servoCenter;
+            float y;
+            // CTRE.GamepadValues val = new CTRE.GamepadValues();
 
             PWM servo = new PWM(CTRE.HERO.IO.Port3.PWM_Pin7, period, duration,
             PWM.ScaleFactor.Microseconds, false);
@@ -28,38 +34,12 @@ namespace Hero_PWM1
             /* loop forever */
             while (true)
             {
-                /* get gamepad info */
-                _gamepad.GetAllValues(ref val);
-                //use the cross controller to change the servo position
-                cross = val.pov;
-                switch (cross)
-                {
-                    case 0:// N
-                        delta = 10;
-                        break;
-                    case 1: // NE
-                        delta = 100;
-                        break;
-                    case 7:// NW
-                        delta = 1;
-                        break;
-                    case 4:// S
-                        delta = -10;
-                        break;
-                    case 3: // SE
-                        delta = -100;
-                        break;
-                    case 5:// SW
-                        delta = -1;
-                        break;
-                    default:
-                        delta = 0;
-                        break;
-                }
-                delta += (int)servo.Duration;
+                /* use gamepad axis to set servo */
+                y = _gamepad.GetAxis(1);
+
+                duration = (uint)(servoLow + (y + 1) / 2 * (servoHi - servoLow));
                 // delta has a new duration.  Limit it and put it back into duration
-                Limit(750, ref delta, 2250); // the range on the servo I'm using is a bit larger than 1000 - 2000
-                servo.Duration = (uint)(delta);
+                servo.Duration = duration;
 
                 /* feed watchdog to keep Talon's enabled if Gamepad is inserted. */
                 // also display data
@@ -72,19 +52,12 @@ namespace Hero_PWM1
                     stringBuilder.Clear();
                 }
                 else
-                {
-                    servo.Duration = 1500;
+                {   // put the servo to the center before the watchdog starves.
+                    servo.Duration = servoCenter;
                 }
                 /* run this task every 20ms */
                 Thread.Sleep(20);
             }
         }
-        static void Limit(int low, ref int x, int high)
-        {
-            if (x < low) x = low;
-            if (x > high) x = high;
-        }
-
     }
-
 }
